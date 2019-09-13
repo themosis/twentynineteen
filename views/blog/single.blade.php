0ca89c05e6c8d40817b4cc1b09dc7e8796e626fe
Julien Lambé <julien@themosis.com>
2019-09-13 07:44:51 +0200

    Start single view implementation.

---
+++ views/blog/single.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.main')
+
+@section('site-content')
+	<section id="primary" class="content-area">
+		<main id="main" class="site-main">
+			@loop
+				@template('template-parts.content.content', 'single');
+
+				@if(is_singular( 'attachment' ))
+					{{-- Parent post navigation. --}}
+					{!! get_the_post_navigation([
+						'prev_text' => sprintf( __('<span class="meta-nav">Published in</span><span class="post-title">%s</span>', THEME_TD), '%title')
+					]) !!}
+				@elseif(is_singular('post'))
+					{{-- Previous/next post navigation. --}}
+					{!! get_the_post_navigation([
+						'next_text' => '<span class="meta-nav" aria-hidden="true">' . __('Next Post', THEME_TD) . '</span> ' .
+							'<span class="screen-reader-text">' . __('Next post:', THEME_TD) . '</span> <br/>' .
+							'<span class="post-title">%title</span>',
+						'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __('Previous Post', THEME_TD) . '</span> ' .
+							'<span class="screen-reader-text">' . __('Previous post:', THEME_TD) . '</span> <br/>' .
+							'<span class="post-title">%title</span>',
+					]) !!}
+				@endif
+
+				{{-- If comments are open or we have at least one comment, load up the comment template. --}}
+				@if(comments_open() || get_comments_number())
+					@php(comments_template('/views/comments/template.php'))
+				@endif
+			@endloop
+		</main><!-- #main -->
+	</section><!-- #primary -->
+@endsection
--- views/comments.php
+++ views/comments.php
@@ -1,29 +1,3 @@
-<?php
-/**
- * The template for displaying comments
- *
- * This is the template that displays the area of the page that contains both the current comments
- * and the comment form.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package WordPress
- * @subpackage Twenty_Nineteen
- * @since 1.0.0
- */
-
-/*
- * If the current post is protected by a password and
- * the visitor has not yet entered the password we will
- * return early without loading the comments.
-*/
-if ( post_password_required() ) {
-	return;
-}
-
-$discussion = twentynineteen_get_discussion_data();
-?>
-
 <div id="comments" class="<?php echo comments_open() ? 'comments-area' : 'comments-area comments-closed'; ?>">
 	<div class="<?php echo $discussion->responses > 0 ? 'comments-title-wrap' : 'comments-title-wrap no-responses'; ?>">
 		<h2 class="comments-title">
